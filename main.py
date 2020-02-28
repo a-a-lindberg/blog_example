16ba90daa333e4ce4763dade99261312e67c099e
Alexey Lindberg <alexey.lindberg@gmail.com>
2020-02-29 00:58:20 +0300

    index page

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from data import db_session
+from data.news import News
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -9,6 +10,13 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def main():
     db_session.global_init("db/blogs.sqlite")
     app.run()
+
+
+@app.route("/")
+def index():
+    session = db_session.create_session()
+    news = session.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
 
 
 if __name__ == '__main__':

6f62bc88c5c3f1f442c6001f683bea9fdd25b4ed
Alexey Lindberg <alexey.lindberg@gmail.com>
2020-02-29 11:10:31 +0300

    register form handler

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from werkzeug.utils import redirect
 
 from data import db_session
 from data.news import News
+from data.users import User
+from forms.register import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -17,6 +20,36 @@ def index():
     session = db_session.create_session()
     news = session.query(News).filter(News.is_private != True)
     return render_template("index.html", news=news)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login')
+def login():
+    return "Извините, эта страница пока не готова. Приходите позже.", 503
 
 
 if __name__ == '__main__':

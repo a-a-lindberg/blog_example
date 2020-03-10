6142292c38818e02c18161990e6b1f9487fce9de
Alexey Lindberg <alexey.lindberg@gmail.com>
2020-03-10 13:08:39 +0300

    return json

--- news_api.py
+++ news_api.py
@@ -1,8 +1,24 @@
 import flask
+from flask import jsonify
+
+from data import db_session
+from data.news import News
 
 blueprint = flask.Blueprint('news_api', __name__, template_folder='templates')
 
 
 @blueprint.route('/api/news')
 def get_news():
-    return 'Обработчик в news_api'
+    session = db_session.create_session()
+    # TODO: Проверить авторизацию пользователя
+    news = session.query(News).all()
+
+    return jsonify(
+        {
+            'news': [
+                item.to_dict(
+                    only=('title', 'content', 'user.name')
+                ) for item in news
+            ]
+        }
+    )

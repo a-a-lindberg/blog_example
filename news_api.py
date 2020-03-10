02aba7434cf88055811a6df03fb94d64f1c9a692
Alexey Lindberg <alexey.lindberg@gmail.com>
2020-03-10 13:53:22 +0300

    create_news

--- news_api.py
+++ news_api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify
+from flask import jsonify, request
 
 from data import db_session
 from data.news import News
@@ -7,7 +7,7 @@ from data.news import News
 blueprint = flask.Blueprint('news_api', __name__, template_folder='templates')
 
 
-@blueprint.route('/api/news')
+@blueprint.route('/api/news', methods=['GET'])
 def get_news():
     session = db_session.create_session()
     # TODO: Проверить авторизацию пользователя
@@ -22,6 +22,30 @@ def get_news():
             ]
         }
     )
+
+
+@blueprint.route('/api/news', methods=['POST'])
+def create_news():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(
+            key in request.json for key in [
+                'title', 'content', 'user_id', 'is_private'
+            ]
+    ):
+        return jsonify({'error': 'Bad request'})
+    # TODO: Проверить авторизацию пользователя
+
+    session = db_session.create_session()
+    news = News(
+        title=request.json['title'],
+        content=request.json['content'],
+        user_id=request.json['user_id'],
+        is_private=request.json['is_private']
+    )
+    session.add(news)
+    session.commit()
+    return jsonify({'success': 'ok'})
 
 
 @blueprint.route('/api/news/<int:news_id>', methods=['GET'])

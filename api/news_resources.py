1b18c5df2cbf2c28559316fcf50961b9f3dff7f7
Alexey Lindberg <alexey.lindberg@gmail.com>
2020-03-13 20:03:27 +0300

    NewsListResource

--- api/news_resources.py
+++ api/news_resources.py
@@ -1,8 +1,15 @@
 from flask import jsonify
-from flask_restful import abort, Resource
+from flask_restful import abort, Resource, reqparse
 
 from data import db_session
 from data.news import News
+
+parser = reqparse.RequestParser()
+parser.add_argument('title', required=True)
+parser.add_argument('content', required=True)
+parser.add_argument('is_private', required=True)
+parser.add_argument('is_published', required=True)
+parser.add_argument('user_id', required=True, type=int)
 
 
 def abort_if_news_not_found(news_id):
@@ -26,5 +33,31 @@ class NewsResource(Resource):
         session = db_session.create_session()
         news = session.query(News).get(news_id)
         session.delete(news)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+
+class NewsListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        news = session.query(News).all()
+        return jsonify({
+            'news': [
+                item.to_dict(
+                    only=('title', 'content', 'user.name')
+                ) for item in news
+            ]
+        })
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        news = News(
+            title=args['title'],
+            content=args['content'],
+            user_id=args['user_id'],
+            is_private=args['is_private']
+        )
+        session.add(news)
         session.commit()
         return jsonify({'success': 'OK'})
